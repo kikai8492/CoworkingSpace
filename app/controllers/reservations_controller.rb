3ec422fae2daa0ed6abd6f1035a649688a4d39e9
shuto kikai <yysskikai04@icloud.com>
2023-11-12 08:15:56 +0900

    過去の日にちに予約できないようにした

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -14,12 +14,13 @@ class ReservationsController < ApplicationController
   def create
     @reservation = current_user.reservations.build(reservation_params)
     if @reservation.reservation_date < Date.today
-      render :new, notice: "過去の日付は予約できません"
-    end
-    if @reservation.save
+      flash[:notice] = "過去の日付は予約できません"
+      render :new
+    elsif @reservation.save
       redirect_to reservations_path, notice: "予約が完了しました"
     else
-      render :new, notice: "予約に失敗しました"
+      flash[:notice] = "予約に失敗しました"
+      render :new
     end
   end
 
@@ -32,9 +33,7 @@ class ReservationsController < ApplicationController
 
   def edit 
     @reservation = Reservation.find(params[:id])
-    if @reservation.reservation_date < Date.today
-      render :edit, notice: "過去の日付は予約できません"
-    end
+
     if @reservation.user_id != current_user.id
       redirect_to reservations_path, notice: "他人の予約は編集できません"
     end
@@ -47,12 +46,20 @@ class ReservationsController < ApplicationController
 
   def update
     @reservation = Reservation.find(params[:id])
-    if @reservation.update(reservation_params)
+    updated_reservation_date = reservation_params[:reservation_date]
+  
+    if updated_reservation_date.present? && Date.parse(updated_reservation_date) < Date.today
+      flash.now[:notice] = "過去の日付は予約できません"
+      render :edit
+    elsif @reservation.update(reservation_params)
       redirect_to reservations_path, notice: "予約を更新しました"
     else
-      render :edit, notice: "予約の更新に失敗しました"
+      flash.now[:notice] = "予約の更新に失敗しました"
+      render :edit
     end
   end
+  
+  
 
   def destroy
     @reservation = Reservation.find(params[:id])
